2ffa12d98af1f8993a3eefcbd6e7e406be2a17a9
asyamincheva <asya.m.mincheva@gmail.com>
2015-10-03 18:25:37 +0300

    Interface IRouter created

---
+++ Framework/Routers/IRouter.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Framework\Routers;
+
+interface IRouter
+{
+    /**
+     * @return 'package/controller/method/param[0]/param[1]
+     */
+    public function getURI();
+
+    public function getPost();
+
+    public function getRequestMethod();
+}
