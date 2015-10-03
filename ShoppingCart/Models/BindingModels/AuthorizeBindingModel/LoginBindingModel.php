9c4878e1fff9d5c968103cf4a6bc70087deef73a
asyamincheva <asya.m.mincheva@gmail.com>
2015-10-04 00:37:08 +0300

    Authorize Binding Model functionality added

---
+++ ShoppingCart/Models/BindingModels/AuthorizeBindingModel/LoginBindingModel.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Models\BindingModels\AuthorizeBindingModel;
+
+class LoginBindingModel
+{
+    private $username;
+    private $password;
+
+    function __construct(array $params)
+    {
+        $this->setUsername($params['username']);
+        $this->setPassword($params['password']);
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $username
+     */
+    private  function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    private  function setPassword($password)
+    {
+        $this->password = $password;
+    }
+}
---
+++ ShoppingCart/Models/BindingModels/AuthorizeBindingModel/RegisterBindingModel.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Models\BindingModels\AuthorizeBindingModel;
+
+class RegisterBindingModel
+{
+    private $username;
+    private $password;
+    private $confirm;
+
+    function __construct(array $params)
+    {
+        $this->setUsername($params['username']);
+        $this->setPassword($params['password']);
+        $this->setConfirm($params['confirm']);
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $username
+     */
+    private function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    private function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    public function getConfirm(){
+        return $this->confirm;
+    }
+
+    private function setConfirm($password)
+    {
+        $this->confirm = $password;
+    }
+}
